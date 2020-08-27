d24027bf594f61ea55f0daa61cfc31a0a0d3c503
michelepapucci <michele.papucci97@gmail.com>
2020-08-27 15:39:23 +0200

    html-css-index-muoiodicovid

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-	echo "sei sulla cazzo di home";
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</head>
+<body class = "home contenitore">
+    <h1>Benvenuto su Grindr!</h1>
+    
+</body>
+</html>
