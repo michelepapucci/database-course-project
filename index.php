d2d23b99f48240cf2c0cb345362b4b53f9ceb68b
michelepapucci <michele.papucci97@gmail.com>
2020-08-27 19:03:30 +0200

    css-html-index-navbar

--- index.php
+++ index.php
@@ -6,7 +6,18 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
-<body class = "home contenitore">
+<body class = "home">
+    <ul class = "navbar">
+        <li class = "navbar_el"><a href="index.php">Home</a></li>
+        <li class = "navbar_el"><a href="news.asp">News</a></li>
+        <li class = "navbar_el"><a href="contact.asp">Contact</a></li>
+        <li class = "navbar_el"><a href="about.asp">About</a></li>
+        <li class = "navbar_el"><form action="/action_page.php">
+            <label for="ricerca">Cerca un blog per nome:</label>
+            <input type="search" class = "navbar_ricerca" id="ricerca" name="ricerca">
+            <input type="submit" class = "navbar_bottone">
+        </form></li>
+    </ul>
     <h1>Benvenuto su Grindr!</h1>
     
 </body>
