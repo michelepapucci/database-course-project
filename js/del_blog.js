f5c16e140038d3181ebfa4612cdf3d1a1f62badb
Michele Papucci <michele.papucci97@gmail.com>
2020-08-30 16:40:29 +0200

    delete-blog-ok

--- js/del_blog.js
+++ js/del_blog.js
@@ -1,7 +1,18 @@
 $(function() {
     $('#cancella_blog').on('click', function() {
-        if(confirm()) {
-
+        if(confirm("ATTENZIONE! Eliminando il blog perderai tutti i post, con le loro immagini e i loro commenti")) {
+            $.ajax({
+                url: "delete_blog.php",
+                success(data){
+                    console.log(data);
+                    if(data == 'ok'){
+                        alert("Blog eliminato correttamente!");
+                        document.location.href="index.php";
+                    } else {
+                        alert("Errore durante l'eliminazione del blog!");
+                    }
+                }
+            });
         }
     });
 });
