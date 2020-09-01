e5e88a1d7759bd73ab4763a60298cf0fae4c5579
Michele Papucci <michele.papucci97@gmail.com>
2020-09-01 19:23:04 +0200

    edit-utente-ok

--- aggiorna_dati_utente.php
+++ aggiorna_dati_utente.php
@@ -1,18 +1,16 @@
 <?php
 	session_start();
-
 	require 'db_handler.php';
 	require 'account.php';
 
 	try{
 		$pdo = db_connect();
 		$account = new Account();
-		$id = $account -> nuovoAccount($_POST["nome_utente"], $_POST["password"], $_POST["email"], $_POST["documento"], $_POST["cellulare"]);
-		$account -> login($_POST["email"], $_POST["password"]);
+		$account -> loginDaSessione();
+		$account -> modificaAccount($_POST["nome_utente"], $_POST["password"], $_POST["email"], $_POST["documento"], $_POST["cellulare"]);
 		header("Location: index.php");
 	} catch (Exception $e) {
 		echo $e ->getMessage();
 	}
 
 	$pdo = null;
-
